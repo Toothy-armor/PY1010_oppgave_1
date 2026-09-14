390621d755eb89fde23fddc0a96431a3c3727100
Toothy-armor <sander.boge@outlook.com>
2026-09-14 21:38:44 +0200

    Refactor code with improved comments
    Updated comments for clarity and added execution section.

--- YearlyCostComparison-0-1-0.py
+++ YearlyCostComparison-0-1-0.py
@@ -17,19 +17,22 @@ GasPricePerKm = 1 # in kr/km
 GasTollPrice = 0.3 #Kr/km
 
 #%%Functions
-def CalcElPerKm(Powerdraw,PowerPrice, TollPrice):
+def CalcElPerKm(Powerdraw,PowerPrice, TollPrice): # calculates cost per km for electric car
     return(Powerdraw*PowerPrice + TollPrice)
 
-def CalcGasPrice(GasPrice,TollPrice):
+def CalcGasPrice(GasPrice,TollPrice): # calculates cost per km for gas car
     return(GasPrice + TollPrice)
 
-def CalcPerYearCost(Insurance,Km,DailyInsurance,PerKmCost):
+def CalcPerYearCost(Insurance,Km,DailyInsurance,PerKmCost): #calculate total yearly cost using per km cost and insurance cost
     return(Km*PerKmCost + Insurance + DailyInsurance*365)
-#%% Execution
+
+#%% Execution and calling functions
 ElPerKm = CalcElPerKm(ElKWhPerKm, EnergyPrice, ElTollPrice)
 GasPerKm = CalcGasPrice(GasPricePerKm, GasTollPrice)
  
 GasPerYear = CalcPerYearCost(GasInsurancePerYear, KmPerYear, InsurancePerDay, GasPerKm)
 ElPerYear = CalcPerYearCost(ElInsurancePerYear, KmPerYear, InsurancePerDay, ElPerKm)
+
+#%%print costs
 print(GasPerYear,"Kr is the price of a gas car per Year")
 print(ElPerYear,"Kr is the price of an electric car per Year")
